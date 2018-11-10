c4f2bcfab643cfd6fd6b4acdfa1da52d086b15ad
Taiju Okuda <hw17a042@oecu.jp>
2018-11-10 20:14:34 +0900

    no message

--- Double_0_7/Assets/Scripts/ButtonPush.cs
+++ Double_0_7/Assets/Scripts/ButtonPush.cs
@@ -6,11 +6,30 @@ public class ButtonPush : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+
+    /// <summary>
+    /// xに数値を代入して色を変えます。x=０が赤色でx＝２が青、x＝３が黄色で、x＝４が緑です。
+    /// </summary>
+    /// <returns>The change.</returns>
+    /// <param name="x">The x coordinate.</param>
+    void Change(int x)
+    {
+        if (x == 0){
+            this.gameObject.GetComponent<Renderer>().material.color = Color.red;
+        }else if (x == 1)
+        {
+            this.gameObject.GetComponent<Renderer>().material.color = Color.blue;
+        }else if(x == 2){
+            this.gameObject.GetComponent<Renderer>().material.color = Color.yellow;
+        }else if(x == 3){
+            this.gameObject.GetComponent<Renderer>().material.color = Color.green;
+        }
+    }
 }
