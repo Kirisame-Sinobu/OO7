84fefdf1049d7a7824570fe80db27e166a950abb
Taiju Okuda <hw17a042@oecu.jp>
2018-12-22 22:31:53 +0900

    0010

---
+++ Double_07/Assets/Script/StateButtom.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateButtom : MonoBehaviour {
+
+
+    private enum build_state
+    {
+        None,
+        Fund,
+        Info,
+        Boss
+    };
+
+    private struct state
+    {
+        public int building { get; set; }
+    }
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
